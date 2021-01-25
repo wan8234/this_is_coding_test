f62fc511647c7e175a3bc8aaaef9db6d155764a9
wan8234 <wan8234@naver.com>
2021-01-25 19:17:51 +0900

    10-5.py practice

---
+++ 10-4.py
@@ -0,0 +1,38 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v, e = map(int, input().split())
+parent = [0] * (v + 1)
+
+for i in range(1, v + 1):
+    parent[i] = i
+
+cycle = False
+
+for i in range(e):
+    a, b = map(int, input().split())
+    if find_parent(parent, a) == find_parent(parent, b):
+        cycle = True
+    else:
+        union_parent(parent, a, b)
+
+if cycle:
+    print('싸이클이 발생했습니다.')
+else:
+    print('싸이클이 발생하지 않았습니다.')
+
+# 입력 예시
+# 3 3
+# 1 2
+# 1 3
+# 2 3
